bee76d42d85c3a1e22c9b5485a46000cbdf5380f
Joseph Freedman <jfreedman340@gmail.com>
2022-10-12 13:26:46 -0700

    I'm saving my progress and will do the rest at home.

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,16 +1,54 @@
+/**
+ * @author Trevor Hartman
+ *
+ * @author Joseph Freedman
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class WhileLoops {
-    private final static Scanner s = new Scanner(System.in);
 
-    public static void eoCount() {
+    public static void eoCount(ArrayList<Integer> numList) {
+        int even=0, odd=0;
+        for(int num : numList) {
+            if(num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.printf("Even Count is %d%nOdd Count is %d%n", even, odd);
     }
 
-    public static void mean() {
+    public static double mean(ArrayList<Integer> numList) {
+        double sum = 0.0;
+
+        for(int num : numList) {
+            // add list values with sum variable one-by-one.
+            sum += num;
+        }
+        double mean = sum / numList.size();
+        System.out.printf("The mean is: %f", mean);
+        return mean;
     }
 
     public static void main(String[] args) {
-        eoCount();
-        mean();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Numbers: ");
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        String l;
+        while(!(l = s.nextLine()).equalsIgnoreCase("Q")) {
+            try {
+                numList.add(Integer.parseInt(l));
+            } catch(NumberFormatException nfe) {}
+            System.out.println("Enter Numbers: ");
+        }
+
+        eoCount(numList);
+        double myMean = mean(numList);
     }
 }
